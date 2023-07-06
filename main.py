573382cd3ecc539c56718f8d2a477eebd360854e
DESKTOP-V1OMAP2\202-4 <rbghks4677@naver.com>
2023-07-06 10:36:56 +0900

    한번더 뽑기

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import random
 
-print("로또 번호 다섯개를 추첨하자!")
+print("로또 번호 다섯개!")
 
 for i in range(5):
     lotto = random.sample(range(1,46),6)
     print(lotto)
+
+y = input("다시 추첨하기 원하면 y를 누르세요:")
+
+if y == "y" :
+    lotto =  random.sample(range(1,46),6)
+    print(lotto)
+
